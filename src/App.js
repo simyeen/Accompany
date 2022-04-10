67877f93cb76e477f18ec0aec731b4390eeafc16
simyeen <simyeen@naver.com>
2022-04-10 15:59:49 +0900

    Add : changing route Fun to using location.hash

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
 import Home from './page/Home/index.js';
-import Board from './page/Board/index.js';
+import Result from './page/Result/index.js';
 import Header from './components/Header/Header.js';
+import Storage from './utils/Storage.js';
+
 export default function App({$target, initialState}) {
+  // const storage = new Storage({key: 'path'});
   const $header = new Header({$target, text: '안녕 나는 헤더'});
 
   const home = Home({$target, $header, initialState});
-  // Board({$target, initialState});
+  const resultPage = Result({$target, $header, initialState});
 
   function route() {
-    const {pathname} = location;
-    const $container = document.querySelector('.app');
+    // let pathname = storage.getItem() ? storage.getItem() : '/';
+    let pathname = location.hash ? location.hash : '/';
+    console.log(pathname);
+    // console.log(pathname, storage, storage.getItem());
+    $target.innerHTML = '';
 
     if (pathname === '/') {
+      $header.render();
       home.render();
-    } else if (pathname === '/it-board') {
-    } else if (pathname === '/welfare-board') {
-    } else if (pathname === '/qna-board') {
+    } else if (pathname === '#result') {
+      console.log('result 페이지 렌더링');
+      resultPage.render();
+    } else if (pathname === '/#it-board') {
+    } else if (pathname === '/#welfare-board') {
+    } else if (pathname === '/#qna-board') {
     }
   }
 
   window.addEventListener('click', event => {
     if (event.target.className === 'it-button') {
-      event.preventDefault();
-
-      const {href} = event.target;
-      const path = href.replace(window.location.origin, '');
-
-      history.pushState(null, null, path);
-      route();
+      // event.preventDefault();
+      // const {href} = event.target;
+      // const path = href.replace(window.location.origin, '');
+      // history.pushState(null, null, path);
+      // console.log(history);
+      // route();
     }
   });
 
-  window.addEventListener('popstate', () => route());
+  window.addEventListener('popstate', () => {
+    console.log('발생');
+    route();
+  });
 
   route();
 }
